6861ae33390ef59213ce30f75b72622cdba0f725
Joey <2504657518@qq.com>
2021-11-17 20:18:52 +0800

    优化模板逻辑

--- src/stories/Button1.stories.js
+++ src/stories/Button1.stories.js
@@ -24,11 +24,6 @@ const Template = (args, { argTypes }) => ({
   components: { JButton },
   template: '<j-button @onClick="onClick" v-bind="$props">{{label}}</j-button>',
 });
-const TemplateForIcon = (args, { argTypes }) => ({
-  props: Object.keys(argTypes),
-  components: { JButton },
-  template: '<j-button @onClick="onClick" v-bind="$props"></j-button>',
-});
 
 export const 类型 = Template.bind({});
 类型.args = {
@@ -47,10 +42,9 @@ export const 圆角 = Template.bind({});
   type: 'primary',
   label: '按钮'
 };
-export const 图标 = TemplateForIcon.bind({});
+export const 图标 = Template.bind({});
 图标.args = {
   icon: 'j-icon-edit',
   type: 'primary',
   circle: true,
-  label: '按钮'
 };
