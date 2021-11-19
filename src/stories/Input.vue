6db9a14946b7ffc7f85193e8bfeb4d0c938c5f07
kiemz <511585052@qq.com>
2021-11-19 15:23:05 +0800

    error

---
+++ src/stories/Input.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="input">
+    <el-input v-model="input" :placeholder="placeholder" 
+    :disabled="disabled" :clearable="clearable"
+    :show-password="showPassword" :type="type"></el-input>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Input',
+  data() { 
+    return {
+      input: ''
+    }
+  },
+  props: {
+    placeholder: { required: false, default: "请输入内容" },
+    disabled: { required: false, default: false },
+    clearable: { required: false, default: false },
+    showPassword: { required: false, default: false },
+    type: { required: false, default: 'input' },
+  },
+  components:{
+
+  },
+  created() {
+
+  },
+  mounted() {
+
+  },
+  updated() {
+
+  },
+  methods:{
+
+  },
+ }
+</script>
+
+<style scoped>
+
+</style>
